906e5682ad5cd069518136aec10413daed6ef3a9
SadDevastator <57295616+SadDevastator@users.noreply.github.com>
2025-12-02 09:48:20 +0200

    Update Arduino template files with setup and loop functions

--- templates/Arduino/Arduino.ino
+++ templates/Arduino/Arduino.ino
@@ -1,1 +1,11 @@
 #include "src/app/main.h"
+
+void setup()
+{
+    app_setup();
+}
+
+void loop()
+{
+    app_loop();
+}
--- templates/Arduino/src/app/main.c
+++ templates/Arduino/src/app/main.c
@@ -1,1 +1,10 @@
 #include "../APP_Cfg.h"
+
+void app_setup()
+{
+    // Initialize application components here
+}
+void app_loop()
+{
+    // Main application loop code here
+}
